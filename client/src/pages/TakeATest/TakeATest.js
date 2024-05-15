33648a1522b4c5ea8454be0479ae2b429568ad80
vdtien2908 <vuductien2908@gmail.com>
2024-05-15 12:33:51 +0700

    [feature] - complete ui page take a test

--- client/src/pages/TakeATest/TakeATest.js
+++ client/src/pages/TakeATest/TakeATest.js
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
 import { FaPowerOff, FaFilePen, FaStopwatch } from 'react-icons/fa6';
+import { RadioButton } from 'primereact/radiobutton';
+import React, { useState } from 'react';
 
 // Style css
 import style from './TakeATest.module.scss';
@@ -8,6 +10,13 @@ import style from './TakeATest.module.scss';
 import Button from '~/components/Button';
 
 function TakeATest() {
+    const answers = [
+        { name: 'Đáp án A', key: 'A' },
+        { name: 'Đáp án B', key: 'M' },
+        { name: 'Đáp án C', key: 'P' },
+        { name: 'Đáp án D', key: 'R' },
+    ];
+    const [selectedAnswer, setSelectedAnswer] = useState([]);
     return (
         <div className={clsx(style.wrapper)}>
             {/*  Topbar */}
@@ -38,6 +47,86 @@ function TakeATest() {
                 </div>
             </div>
             {/* /Topbar */}
+
+            {/* Content */}
+            <div className={clsx(style.content)}>
+                {/* Left content */}
+                <div className={clsx(style.left_content)}>
+                    <div className={clsx(style.question)}>
+                        <div className={clsx(style.question_content)}>
+                            <div className={clsx(style.question_number)}>
+                                <b>Câu 1</b>
+                                <p>Đâu là cách khai báo một biến trong PHP?</p>
+                            </div>
+                            <div className={clsx(style.answer_require)}>
+                                Chọn một đáp án đúng
+                            </div>
+                            <div className={clsx(style.question_answer)}>
+                                {answers.map((answer) => {
+                                    return (
+                                        <div
+                                            key={answer.key}
+                                            className={clsx(style.answer)}
+                                        >
+                                            <RadioButton
+                                                inputId={answer.key}
+                                                name="answer"
+                                                value={answer}
+                                                onChange={(e) =>
+                                                    setSelectedAnswer(e.value)
+                                                }
+                                                checked={
+                                                    selectedAnswer.key ===
+                                                    answer.key
+                                                }
+                                            />
+                                            <label
+                                                htmlFor={answer.key}
+                                                className="ml-2"
+                                            >
+                                                {answer.name}
+                                            </label>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* /Left content */}
+
+                {/* Right content */}
+                <div className={clsx(style.right_content)}>
+                    <div className={clsx(style.list_question_container)}>
+                        <h2 className={clsx(style.list_question_title)}>
+                            Danh sách câu hỏi
+                        </h2>
+                        <div className={clsx(style.list_question)}>
+                            <div
+                                className={clsx(
+                                    style.number_question,
+                                    style.selected
+                                )}
+                            >
+                                1
+                            </div>
+                            <div className={clsx(style.number_question)}>2</div>
+                            <div className={clsx(style.number_question)}>3</div>
+                            <div className={clsx(style.number_question)}>4</div>
+                            <div className={clsx(style.number_question)}>5</div>
+                            <div className={clsx(style.number_question)}>6</div>
+                            <div className={clsx(style.number_question)}>7</div>
+                            <div className={clsx(style.number_question)}>8</div>
+                            <div className={clsx(style.number_question)}>9</div>
+                            <div className={clsx(style.number_question)}>
+                                10
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* /Right content */}
+            </div>
+            {/* /Content */}
         </div>
     );
 }
